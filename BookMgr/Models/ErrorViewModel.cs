b121c59985813538ca9d9c629bc2c773d0e867c5
Simanga Shabalala <simangae@gmail.com>
2024-08-19 08:28:55 +0200

    Add project files.

---
+++ BookMgr/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookMgr.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
