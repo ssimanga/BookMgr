6337c76c25cac1edd879e67247e6b2d5545ddd6b
Simanga Shabalala <simangae@gmail.com>
2024-08-20 08:57:06 +0200

    Added Models and Migrations

--- BookMgr/Data/ApplicationDbContext.cs
+++ BookMgr/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BookMgr.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookMgr.Data
@@ -8,6 +9,26 @@ namespace BookMgr.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<BookAuthor> BookAuthors { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<BookAuthor>()
+                 .HasKey(ba => new { ba.BookId, ba.AuthorId });
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne(ba => ba.Book)
+                .WithMany(b => b.BookAuthors)
+                .HasForeignKey(ba => ba.BookId);
+
+            modelBuilder.Entity<BookAuthor>()
+                .HasOne(ba => ba.Author)
+                .WithMany(a => a.BookAuthors)
+                .HasForeignKey(ba => ba.AuthorId);
         }
     }
 }
---
+++ BookMgr/Models/Publisher.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookMgr.Models
+{
+    public class Publisher
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+}
