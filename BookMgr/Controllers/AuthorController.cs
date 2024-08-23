fb66588ba05b14da33b9a4a2512019b9aca1e2bc
Simanga Shabalala <simangae@gmail.com>
2024-08-23 08:50:42 +0200

    Publisher Controller

--- BookMgr/Controllers/AuthorController.cs
+++ BookMgr/Controllers/AuthorController.cs
@@ -52,5 +52,83 @@ namespace BookMgr.Controllers
             }
             return View(author);
         }
+        //show form to edit an existing author
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var author = await _context.Authors.FindAsync(id);
+            if(author == null)
+            {
+                return NotFound();
+            }
+            return View(author);
+        }
+        //save the edited author to the database
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Author author)
+        {
+            if(id != author.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(author);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AuthorExists(author.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(author);
+        }
+
+        //show confrimation page to delete an author
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var author = await _context.Authors.FirstOrDefaultAsync(m => m.Id == id);
+            if(author == null)
+            {
+                return NotFound();
+            }
+            return View(author);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var author = await _context.Authors.FindAsync(id);
+            if(author != null)
+            {
+                _context.Authors.Remove(author);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AuthorExists(int id)
+        {
+            return _context.Authors.Any(e => e.Id == id);
+        }
     }
 }
--- BookMgr/Controllers/PublisherController.cs
+++ BookMgr/Controllers/PublisherController.cs
@@ -1,12 +1,133 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookMgr.Data;
+using BookMgr.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookMgr.Controllers
 {
     public class PublisherController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        public PublisherController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var publishers = await _context.Publishers.ToListAsync();
+            return View(publishers);
+        }
+        //View details of a specific author
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var publisher = await _context.Publishers
+                                .Include(a => a.Books)
+                                .FirstOrDefaultAsync(m => m.Id == id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+            return View(publisher);
+        }
+        //show the form to create a new author
+        public IActionResult Create()
         {
             return View();
+        }
+        //Save the author to the database
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Publisher publisher)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(publisher);
+               await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(publisher);
+        }
+        //show form to edit an existing publisher
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var publisher = await _context.Publishers.FindAsync(id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+            return View(publisher);
+        }
+        //save the edited publisher to the database
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Publisher publisher)
+        {
+            if (id != publisher.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(publisher);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PublisherExists(publisher.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(publisher);
+        }
+
+        //show confrimation page to delete an author
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var publisher = await _context.Publishers.FirstOrDefaultAsync(m => m.Id == id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+            return View(publisher);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var publisher = await _context.Publishers.FindAsync(id);
+            if (publisher != null)
+            {
+                _context.Publishers.Remove(publisher);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PublisherExists(int id)
+        {
+            return _context.Publishers.Any(e => e.Id == id);
         }
     }
 }
