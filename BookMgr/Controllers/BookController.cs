ecbcc607d4b26d0ac6eb6a5d1e4b077479c62936
Simanga Shabalala <simangae@gmail.com>
2024-08-28 08:54:56 +0200

    Book Authors tables fixed

--- BookMgr/Controllers/BookController.cs
+++ BookMgr/Controllers/BookController.cs
@@ -3,6 +3,7 @@ using BookMgr.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Mono.TextTemplating;
 
 namespace BookMgr.Controllers
 {
@@ -13,71 +14,118 @@ namespace BookMgr.Controllers
         {
             _context = context;
         }
-        public async Task<IActionResult> Index()
-        {
-          var books = await _context.Books
-                    .Include(b => b.Publisher)
-                    .Include(b => b.BookAuthors)
-                    .ThenInclude(ba => ba.Author)
-                    .ToListAsync();
-            var bookViewModels = books.Select(book => new BookViewModel
-            {
-                Id = book.Id,
-                Title = book.Title,
-                ISBN = book.ISBN,
-                PublicationDate = book.PublicationDate,
-                PublisherName = book.Publisher.Name,
-                AuthorNames = book.BookAuthors.Select(ba => ba.Author.Name).ToList()
+        //public async Task<IActionResult> Index()
+        //{
+        //  var books = await _context.Books
+        //            .Include(b => b.Publisher)
+        //            .Include(b => b.BookAuthors)
+        //            .ThenInclude(ba => ba.Author)
+        //            .ToListAsync();
+        //    var bookViewModels = books.Select(book => new BookViewModel
+        //    {
+        //        Id = book.Id,
+        //        Title = book.Title,
+        //        ISBN = book.ISBN,
+        //        PublicationDate = book.PublicationDate,
+        //        PublisherName = book.Publisher.Name,
+        //        AuthorIds = book.BookAuthors.Select(ba => ba.Author.Name).ToList()
 
-            }).ToList();
+        //    }).ToList();
            
-            return View(bookViewModels);
-        }
+        //    return View(bookViewModels);
+        //}
 
         //Details : View details of a specific book
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-            var book = await _context.Books.Include(b => b.Publisher)
-                                            .Include(b => b.BookAuthors)
-                                            .ThenInclude(ba => ba.Author)
-                                            .FirstOrDefaultAsync(m => m.Id == id);
-            if (book == null)
-            {
-                return NotFound();
-            }
-            return View(book);
-        }
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    var book = await _context.Books.Include(b => b.Publisher)
+        //                                    .Include(b => b.BookAuthors)
+        //                                    .ThenInclude(ba => ba.Author)
+        //                                    .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (book == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(book);
+        //}
         //Show the form to create a new book
         public IActionResult Create()
         {
-            ViewBag.PublisherId = new SelectList(_context.Publishers, "Id", "Name");
-            ViewBag.Authors = new MultiSelectList(_context.Authors, "Id", "Name");
-            return View();
+            var viewModel = new BookViewModel
+            {
+                Publishers = _context.Publishers
+                      .Select(p => new SelectListItem
+                      {
+                          Value = p.Id.ToString(),
+                          Text = p.Name
+                      }).ToList(),
+                Authors = _context.Authors
+                   .Select(a => new SelectListItem
+                   {
+                       Value = a.Id.ToString(),
+                       Text = a.Name
+                   }).ToList()
+            };
+            return View(viewModel);
         }
         //Save the new book to the database
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Book book, int[] selectedAuthors)
+        public async Task<IActionResult> Create(BookViewModel viewModel)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                _context.Add(book);
-                await _context.SaveChangesAsync();
-                foreach (var authoirId in selectedAuthors)
+                foreach (var key in ModelState.Keys)
                 {
-                    var bookAuthor = new BookAuthor { BookId = book.Id, AuthorId = authoirId };
-                    _context.Add(bookAuthor);
+                    var state = ModelState[key];
+                    foreach(var error in state.Errors)
+                    {
+                        Console.WriteLine($"Key: {key}, Error{error.ErrorMessage}");
+                    }
                 }
+                //Map the viewModel to the book 
+                var book = new Book
+                {
+                    Title = viewModel.Title,
+                    ISBN = viewModel.ISBN,
+                    PublicationDate = viewModel.PublicationDate,
+                    PublisherId = viewModel.PublisherId
+                };
+
+               _context.Books.Add(book);
+                await _context.SaveChangesAsync();
+
+                foreach (var authorId in viewModel.AuthorIds)
+                {
+                    var bookAuthor = new BookAuthor
+                    {
+                        BookId = book.Id,
+                        AuthorId = authorId
+                    };
+                    _context.BookAuthors.Add(bookAuthor);
+                }
+                 
+               
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewBag.PublisherId = new SelectList(_context.Publishers, "Id", "Name", book.PublisherId);
-            ViewBag.Authors = new MultiSelectList(_context.Authors, "Id", "Name", selectedAuthors);
-            return View(book);
+            viewModel.Publishers = _context.Publishers
+                       .Select(p => new SelectListItem
+                       {
+                           Value = p.Id.ToString(),
+                           Text = p.Name
+                       }).ToList();
+            viewModel.Authors = _context.Authors
+                   .Select(a => new SelectListItem
+                   {
+                       Value = a.Id.ToString(),
+                       Text = a.Name
+                   }).ToList();
+            return View(viewModel);
 
         }
     } 
